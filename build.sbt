9a50c4a3476bf4de72bb58098d2800dabaebce72
Michael Doronin <m.doronin@kryptonite.ru>
2021-11-23 14:19:08 +0300

    Submitting application to spark is working

--- build.sbt
+++ build.sbt
@@ -1,15 +1,17 @@
 name := "SparkSeaweedExample"
 version := "0.1"
 scalaVersion := "2.12.10"
+enablePlugins(PackPlugin)
 
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "3.1.2",
   "org.apache.spark" %% "spark-sql" % "3.1.2",
-  "org.apache.hadoop" % "hadoop-core" % "1.2.1",
-  "org.apache.hadoop" % "hadoop-aws" % "3.2.0",
-  "org.apache.hadoop" % "hadoop-common" % "3.2.0",
-  "org.apache.hadoop" % "hadoop-client" % "3.2.0",
-  "com.github.chrislusf" % "seaweedfs-client" % "1.6.9"
-  /* "com.github.chrislusf" % "seaweedfs-hadoop2-client" % "1.6.9" */
+  /* "org.apache.hadoop" % "hadoop-core" % "1.2.1", */
+  /* "org.apache.hadoop" % "hadoop-aws" % "3.2.0", */
+  /* "org.apache.hadoop" % "hadoop-common" % "3.2.0", */
+  /* "org.apache.hadoop" % "hadoop-client" % "3.2.0", */
+  // "com.amazonaws" % "aws-java-sdk" % "1.12.115",
+  // "com.github.chrislusf" % "seaweedfs-client" % "1.6.9"
+  "com.github.chrislusf" % "seaweedfs-hadoop3-client" % "1.7.0"
 )
--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -8,19 +8,29 @@ object Hello {
     println(args)
     val sparkConf = new SparkConf()
      .setAppName("SparkSeaweedExample")
-     .setMaster("local[1]")
-     // .setMaster("spark://spark:7077")
+     // .setMaster("local[1]")
+     .setMaster("spark://spark:7077")
       // .setJars(Seq("/Users/m/code/spark-seaweedfs/lib/seaweedfs-hadoop2-client-1.6.9.jar"))
      .set("spark.hadoop.fs.seaweedfs.impl", "seaweed.hdfs.SeaweedFileSystem")
-     // .set("spark.hadoop.fs.seaweedfs.impl", "seaweed.hdfs.SeaweedFileSystem")
-     .set("spark.hadoop.fs.defaultFS", "s3a://seaweedfs:8888")
+     .set("spark.hadoop.fs.defaultFS", "seaweedfs://seaweedfs:8888")
+     // .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+     // .set("spark.hadoop.fs.s3a.endpoint", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+     // .set("spark.hadoop.fs.s3a.access.key", "")
+     // .set("spark.hadoop.fs.s3a.secret.key", "")
     // .set("spark.dynamicAllocation.enabled", "false")
     // val sc = new SparkContext(sparkConf)
     // val hadoopConfig = sc.hadoopConfiguration
     // hadoopConfig.set("fs.file.impl", classOf[org.apache.hadoop.fs.LocalFileSystem].getName)
-    val spark = SparkSession.builder.config(sparkConf).getOrCreate()
     // val hadoopConfig = spark.SparkContext.hadoopConfiguration
-    val df = spark.read.format("binaryFile").option("pathGlobFilter", "*.txt").load("s3a://seaweedfs:8888/test-data/")
+    val spark = SparkSession.builder.config(sparkConf).getOrCreate()
+    val df = spark
+      .read
+      .format("binaryFile")
+      .option("pathGlobFilter", "*.txt")
+      .load("seaweedfs://seaweedfs:8888/test-data/")
+      // .load("/Users/m/code/seaweedfs/")
+    df.explain()
     df.select(functions.sum(functions.length(col("content")))).foreach(tb => println(s"total bytes $tb"))
+    spark.close()
   }
 }
