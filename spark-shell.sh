b4b0ec9a13b592bc9383ff3a70c60cdb04195b85
Michael Doronin <m.doronin@kryptonite.ru>
2021-11-24 17:38:56 +0300

    RDD wip

--- spark-shell.sh
+++ spark-shell.sh
@@ -7,6 +7,4 @@ SPARK_PATH=${ARG:-/Users/m/code/spark-play/spark-3.1.2-bin-hadoop2.7}
     --conf spark.hadoop.fs.seaweedfs.impl=seaweed.hdfs.SeaweedFileSystem \
     --conf spark.hadoop.fs.defaultFS=seaweedfs://seaweedfs:8888 \
     --conf spark.driver.extraClassPath="$SPARK_PATH/seaweedfs-hadoop2-client-1.6.9.jar" \
-    --conf spark.driver.bindAddress=127.0.0.1 \
-    --master spark://localhost:7077 \
-    -i ./bench.scala
+    --master spark://localhost:7077
--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -1,6 +1,54 @@
-import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{Partition, SparkConf, SparkContext, TaskContext}
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.{SparkSession, functions}
+
+import java.net.{HttpURLConnection, URL}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.{Duration, SECONDS}
+import scala.concurrent.{Await, Future}
+
+case class Chunk(index: Int, fid: String, vid: Int, path: String)
+
+class VolumePartition(volumeId: Int) extends Partition {
+  override def index: Int = volumeId
+}
+
+
+class SeaweedRDD(
+                sc: SparkContext,
+                paths: Set[String],
+                chunksInfo: Map[Int, Chunk],
+                volumesInfo: Map[Int, String],
+                ) extends RDD[(String, Array[Byte])](sc, Seq()) {
+
+  private val m = volumesInfo.keys.toArray
+
+  override def compute(split: Partition, context: TaskContext): Iterator[(String, Array[Byte])] = {
+    val i = volumesInfo.keys.toArray.apply(split.index)
+    val url = volumesInfo.get(i).get
+    chunksInfo
+      .filter(kv => kv._1 == m(split.index) && paths.contains(kv._2.path))
+      .values
+      .toSeq
+      .sortBy(ch => {ch.path})
+      .map(chunk => (chunk.path, Await.result(loadChunk(url, chunk.fid), Duration.create(5, SECONDS))))
+      .iterator
+  }
+
+  def loadChunk(url: String, fid: String): Future[Array[Byte]] = Future {
+    val connection = new URL(s"http://$url/$fid").openConnection().asInstanceOf[HttpURLConnection]
+    connection.setConnectTimeout(5000)
+    connection.setReadTimeout(5000)
+    connection.connect()
+    connection.getInputStream.readAllBytes()
+  }
+
+  override protected def getPartitions: Array[Partition] = {
+    volumesInfo.keys.toArray.indices.map(i => new Partition { val index: Int = i}).toArray
+  }
+
+}
 
 
 object Hello {
@@ -8,8 +56,8 @@ object Hello {
     println(args)
     val sparkConf = new SparkConf()
      .setAppName("SparkSeaweedExample")
-     // .setMaster("local[1]")
-     .setMaster("spark://spark:7077")
+     .setMaster("local[1]")
+     // .setMaster("spark://spark:7077")
       // .setJars(Seq("/Users/m/code/spark-seaweedfs/lib/seaweedfs-hadoop2-client-1.6.9.jar"))
      .set("spark.hadoop.fs.seaweedfs.impl", "seaweed.hdfs.SeaweedFileSystem")
      .set("spark.hadoop.fs.defaultFS", "seaweedfs://seaweedfs:8888")
@@ -23,14 +71,21 @@ object Hello {
     // hadoopConfig.set("fs.file.impl", classOf[org.apache.hadoop.fs.LocalFileSystem].getName)
     // val hadoopConfig = spark.SparkContext.hadoopConfiguration
     val spark = SparkSession.builder.config(sparkConf).getOrCreate()
-    val df = spark
-      .read
-      .format("binaryFile")
-      .option("pathGlobFilter", "*.txt")
-      .load("seaweedfs://seaweedfs:8888/test-data/")
+    val rdd = new SeaweedRDD(
+      sc = spark.sparkContext,
+      paths = Set("/buckets/test-bucket/vk/_3528/_1747/4"),
+      chunksInfo = Map((4, Chunk(index = 0, fid = "4,214fcca9438d", vid = 4, path = "/buckets/test-bucket/vk/_3528/_1747/4"))),
+      volumesInfo = Map((4, "192.168.105.100:8082"))
+    )
+    rdd.collect().foreach(kv => println(s"key: ${kv._1}, value: ${kv._2.map("%02X" format _).mkString}"))
+    // val df = spark
+    //   .read
+    //   .format("binaryFile")
+    //   .option("pathGlobFilter", "*")
+    //   .load("seaweedfs://seaweedfs:8888/test-bucket/vk/_1824/_6111/")
       // .load("/Users/m/code/seaweedfs/")
-    df.explain()
-    df.select(functions.sum(functions.length(col("content")))).foreach(tb => println(s"total bytes $tb"))
+    // df.explain()
+    // df.select(functions.sum(functions.length(col("content")))).foreach(tb => println(s"total bytes $tb"))
     spark.close()
   }
 }
